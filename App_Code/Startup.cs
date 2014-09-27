9ad5cf52ae5347f4ba6cadb9fc2395e99f0f22a9
Tom Cocozzello <thomas.cocozzello@my.ndsu.edu>
2014-09-26 22:40:13 -0500

    first commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Handstand.Startup))]
+namespace Handstand
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
